508e847cf5fbc1962296ee953f84170b2e8cefbf
李佐宁 <lizn@yjhealth.cn>
2021-07-09 14:47:17 +0800

    fix: 初始化

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-js create app
+## Quickstart
+
+- Via CDN: `<script src="https://unpkg.com/vue@next"></script>`
+- In-browser playground on [Codepen](https://codepen.io/yyx990803/pen/OJNoaZL)
+- Scaffold via [Vite](https://github.com/vitejs/vite):
+
+  ```bash
+  npm install || yarn install
+
+  js create <project_name>
+  ```
+# Keywords
+js, cli, 脚手架
--- bin/index.js
+++ bin/index.js
@@ -3,13 +3,15 @@ const program = require('commander')
 
 const update = require('../lib/update')
 
+const addProject = require('../lib/addProject')
+
 const setMirro = require('../lib/mirro')
 
 const defConfig = require('../lib/config')
 
 const download = require('../lib/download')
 
-const init = require('../lib/init')
+const init = require('../lib/init.copy')
 
 program.version(require('../package.json').version, '-v, --version')
 
@@ -21,7 +23,11 @@ program.command('mirror <set_mirror>').description('Set template mirror').action
 
 program.command('download').description('download template').action(() => download())
 
-program.command('init <set_project_name>').description('init project').action(name => init(name));
-program.command('create <set_project_name>').description('init project').action(name => init(name));
+// packages目录下添加项目
+program.command('add <package_name').description('add project in packages').action((name) => {
+  addProject(name)
+})
+
+// program.command('init <set_project_name>').alias('create').description('init project').action(name => init(name));
 
 program.parse(process.argv)
